27b0f52ec1ff85ddee80f89d9c9c1daa3343cbce
Alexandre Attia <alexandre.attia@wanadoo.fr>
2017-06-22 16:41:31 -0700

    create script to get coordinates of bounding boxes for characters

--- label_pointer.py
+++ label_pointer.py
@@ -1,34 +1,58 @@
+"""
+Label pictures and get bounding boxes coordinates (upper left and lower right).
+Using mouse click we can get those coordinates.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import glob
+from random import shuffle
+import os
+from train import map_characters
 
-n_elem = 5
+# List of already labeled pictures
 with open('./annotation.txt') as f:
     already_labeled = [k.strip().split(' ')[0] for k in f.readlines()]
 
-target = open('annotation.txt', 'a')
-pics = glob.glob('./characters/milhouse_van_houten/*.*')
-for p in pics[:10]:
-    if p not in already_labeled:
-        im = cv2.imread(p)
-        ax = plt.gca()
-        fig = plt.gcf()
-        implot = ax.imshow(im)
+characters = map_characters.values()
+shuffle(characters)
 
-        position = []
-        def onclick(event):
-            if event.xdata != None and event.ydata != None:
-                position.append((event.xdata, event.ydata)) 
-                n_clicks = len(position)
-                if n_clicks == 2:
-                    line = '{0} {1} {2}'.format(p, 
-                        ' '.join([str(int(k)) for k in position[0]]), 
-                        ' '.join([str(int(k)) for k in position[1]])) 
-                    target.write(line)
-                    target.write("\n")
-                    plt.close()   
-        cid = fig.canvas.mpl_connect('button_press_event', onclick)
-        plt.show()
-plt.close()
+for char in characters:
+    pics = glob.glob('./characters/%s/*.*' % char)
+    for p in pics:
+        if p not in already_labeled:
+            im = cv2.imread(p)
+            
+            ax = plt.gca()
+            fig = plt.gcf()
+            implot = ax.imshow(im)
+            position = []
+            def onclick(event):
+                """
+                If click, add the mouse position to the list.
+                Closing the plotted picture after 2 clicks (= 2 cornes.)
+                Write the position for each picture into the text file.
+                """
+                if event.xdata != None and event.ydata != None:
+                    position.append((event.xdata, event.ydata))
+                    n_clicks = len(position)
+                    if n_clicks == 2:
+                        if position[0] == position[1]:
+                            r = input('Delete this picture[Y/n] ? ')
+                            if r.lower() in ['yes','y']:
+                                os.remove(p)
+                                return
+                        line = '{0} {1} {2}'.format(p, 
+                            ' '.join([str(int(k)) for k in position[0]]), 
+                            ' '.join([str(int(k)) for k in position[1]])) 
+
+                        # Open the annotations file to continue to write
+                        target = open('annotation.txt', 'a')
+                        # Write picture and coordinates
+                        target.write(line)
+                        target.write("\n")
+                        plt.close()   
+            cid = fig.canvas.mpl_connect('button_press_event', onclick)
+            plt.show()
+    plt.close()
 
