65bcab60ab40d3dc71b3a78d0a458267c67c66dd
Alexandre Attia <alexandre.attia@wanadoo.fr>
2017-06-27 12:41:53 -0700

    Faster R-CNN from yhennon, slightly edited

---
+++ faster_rcnn/parser.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import random
+import pprint
+
+def get_data(input_path):
+    all_imgs = {}
+    classes_count = {}
+    class_mapping = {}
+    with open(input_path,'r') as f:
+        print('Parsing annotation files')
+        for line in f:
+            line_split = line.strip().split(',')
+            (filename,x1,y1,x2,y2,class_name) = line_split
+
+            if class_name not in classes_count:
+                classes_count[class_name] = 1
+            else:
+                classes_count[class_name] += 1
+
+            if class_name not in class_mapping:
+                class_mapping[class_name] = len(class_mapping)
+
+            if filename not in all_imgs:
+                all_imgs[filename] = {}
+                
+                img = cv2.imread(filename)
+                (rows,cols) = img.shape[:2]
+                all_imgs[filename]['filepath'] = filename
+                all_imgs[filename]['width'] = cols
+                all_imgs[filename]['height'] = rows
+                all_imgs[filename]['bboxes'] = []
+                if np.random.randint(0,6) > 0:
+                    all_imgs[filename]['imageset'] = 'trainval'
+                else:
+                    all_imgs[filename]['imageset'] = 'test'
+
+            all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
+
+        all_data = []
+        for key in all_imgs:
+            all_data.append(all_imgs[key])
+        
+        classes_count['bg'] = 0
+        class_mapping['bg'] = len(class_mapping)
+        random.shuffle(all_data)
+        print('Training images per class ({} classes) :'.format(len(classes_count)))
+        pprint.pprint(classes_count)
+        return all_data, classes_count, class_mapping
+
+
---
+++ train_frcnn.py
@@ -0,0 +1,181 @@
+from __future__ import division
+import random
+import pprint
+import sys
+import time
+import numpy as np
+from optparse import OptionParser
+import pickle
+
+from keras import backend as K
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers import Input
+from keras.models import Model
+from faster_rcnn import config, data_generators
+from faster_rcnn import losses as losses
+from faster_rcnn import resnet as nn
+from faster_rcnn.parser import get_data
+import faster_rcnn.roi_helpers as roi_helpers
+from keras.utils import generic_utils
+
+sys.setrecursionlimit(40000)
+
+parser = OptionParser()
+parser.add_option("-p", "--path", dest="train_path", help="Path to training data.")
+parser.add_option("--hf", dest="horizontal_flips", help="Augment with horizontal flips in training. (Default=false).", action="store_true", default=False)
+parser.add_option("--vf", dest="vertical_flips", help="Augment with vertical flips in training. (Default=false).", action="store_true", default=False)
+parser.add_option("--rot", "--rot_90", dest="rot_90", help="Augment with 90 degree rotations in training. (Default=false).",
+                  action="store_true", default=False)
+(options, args) = parser.parse_args()
+
+if not options.train_path:   # if filename is not given
+    parser.error('Error: path to training data must be specified. Pass --path to command line')
+
+# pass the settings from the command line, and persist them in the config object
+C = config.Config()
+C.use_horizontal_flips = bool(options.horizontal_flips)
+C.use_vertical_flips = bool(options.vertical_flips)
+C.rot_90 = bool(options.rot_90)
+
+all_imgs, classes_count, class_mapping = get_data(options.train_path)
+C.class_mapping = class_mapping
+
+with open(C.config_filename, 'wb') as config_f:
+    pickle.dump(C,config_f)
+    print('Config has been written to {}, and can be loaded when testing to ensure correct results'.format(C.config_filename))
+
+train_imgs = [s for s in all_imgs if s['imageset'] == 'trainval']
+val_imgs = [s for s in all_imgs if s['imageset'] == 'test']
+
+print('Train samples {}, Val samples {}'.format(len(train_imgs), len(val_imgs)))
+
+data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, C, K.image_dim_ordering(), mode='train')
+data_gen_val = data_generators.get_anchor_gt(val_imgs, classes_count, C, K.image_dim_ordering(), mode='val')
+
+img_input = Input(shape=(None, None, 3))
+roi_input = Input(shape=(C.num_rois, 4))
+
+# define the base network (resnet here, can be VGG, Inception, etc)
+shared_layers = nn.nn_base(img_input, trainable=True)
+
+# define the RPN, built on the base layers
+num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+rpn = nn.rpn(shared_layers, num_anchors)
+
+classifier = nn.classifier(shared_layers, roi_input, C.num_rois, nb_classes=len(classes_count), trainable=True)
+
+model_rpn = Model(img_input, rpn[:2])
+model_classifier = Model([img_input, roi_input], classifier)
+
+# this is a model that holds both the RPN and the classifier, used to load/save weights for the models
+model_all = Model([img_input, roi_input], rpn[:2] + classifier)
+
+model_rpn.compile(optimizer=Adam(lr=1e-4), loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
+model_classifier.compile(optimizer=Adam(lr=1e-4), loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+model_all.compile(optimizer='sgd', loss='mae')
+
+epoch_length = 1000
+num_epochs = 2000
+iter_num = 0
+
+losses = np.zeros((epoch_length, 5))
+rpn_accuracy_rpn_monitor, rpn_accuracy_for_epoch = [], []
+start_time = time.time()
+
+best_loss = np.Inf
+
+for epoch_num in range(num_epochs):
+    progbar = generic_utils.Progbar(epoch_length)
+    print('Epoch {}/{}'.format(epoch_num + 1, num_epochs))
+    while True:
+        try:
+            if len(rpn_accuracy_rpn_monitor) == epoch_length and C.verbose:
+                mean_overlapping_bboxes = float(sum(rpn_accuracy_rpn_monitor))/len(rpn_accuracy_rpn_monitor)
+                rpn_accuracy_rpn_monitor = []
+                print('Average number of overlapping bounding boxes from RPN = {} for {} previous iterations'.format(mean_overlapping_bboxes, epoch_length))
+                if mean_overlapping_bboxes == 0:
+                    print('RPN is not producing bounding boxes that overlap the ground truth boxes. Check RPN settings or keep training.')
+
+            X, Y, img_data = next(data_gen_train)
+            loss_rpn = model_rpn.train_on_batch(X, Y)
+            P_rpn = model_rpn.predict_on_batch(X)
+            R = roi_helpers.rpn_to_roi(P_rpn[0], P_rpn[1], C, K.image_dim_ordering(), use_regr=True, overlap_thresh=0.7, max_boxes=300)
+
+            # note: calc_iou converts from (x1,y1,x2,y2) to (x,y,w,h) format
+            X2, Y1, Y2 = roi_helpers.calc_iou(R, img_data, C, class_mapping)
+
+            neg_samples = np.where(Y1[0, :, -1] == 1)
+            pos_samples = np.where(Y1[0, :, -1] == 0)
+
+            if len(neg_samples) > 0:
+                neg_samples = neg_samples[0]
+            else:
+                neg_samples = []
+
+            if len(pos_samples) > 0:
+                pos_samples = pos_samples[0]
+            else:
+                pos_samples = []
+
+            rpn_accuracy_rpn_monitor.append(len(pos_samples))
+            rpn_accuracy_for_epoch.append((len(pos_samples)))
+
+            if len(pos_samples) < C.num_rois//2:
+                selected_pos_samples = pos_samples.tolist()
+            else:
+                selected_pos_samples = np.random.choice(pos_samples, C.num_rois//2, replace=False).tolist()
+            try:
+                selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=False).tolist()
+            except:
+                selected_neg_samples = np.random.choice(neg_samples, C.num_rois - len(selected_pos_samples), replace=True).tolist()
+
+            sel_samples = selected_pos_samples + selected_neg_samples
+
+            loss_class = model_classifier.train_on_batch([X, X2[:, sel_samples, :]], [Y1[:, sel_samples, :], Y2[:, sel_samples, :]])
+
+            losses[iter_num, 0] = loss_rpn[1]
+            losses[iter_num, 1] = loss_rpn[2]
+
+            losses[iter_num, 2] = loss_class[1]
+            losses[iter_num, 3] = loss_class[2]
+            losses[iter_num, 4] = loss_class[3]
+
+            iter_num += 1
+
+            progbar.update(iter_num, [('rpn_cls', np.mean(losses[:iter_num, 0])), ('rpn_regr', np.mean(losses[:iter_num, 1])),
+                                      ('detector_cls', np.mean(losses[:iter_num, 2])), ('detector_regr', np.mean(losses[:iter_num, 3]))])
+
+            if iter_num == epoch_length:
+                loss_rpn_cls = np.mean(losses[:, 0])
+                loss_rpn_regr = np.mean(losses[:, 1])
+                loss_class_cls = np.mean(losses[:, 2])
+                loss_class_regr = np.mean(losses[:, 3])
+                class_acc = np.mean(losses[:, 4])
+
+                mean_overlapping_bboxes = float(sum(rpn_accuracy_for_epoch)) / len(rpn_accuracy_for_epoch)
+                rpn_accuracy_for_epoch = []
+
+                if C.verbose:
+                    print('Mean number of bounding boxes from RPN overlapping ground truth boxes: {}'.format(mean_overlapping_bboxes))
+                    print('Classifier accuracy for bounding boxes from RPN: {}'.format(class_acc))
+                    print('Loss RPN classifier: {}'.format(loss_rpn_cls))
+                    print('Loss RPN regression: {}'.format(loss_rpn_regr))
+                    print('Loss Detector classifier: {}'.format(loss_class_cls))
+                    print('Loss Detector regression: {}'.format(loss_class_regr))
+                    print('Elapsed time: {}'.format(time.time() - start_time))
+
+                curr_loss = loss_rpn_cls + loss_rpn_regr + loss_class_cls + loss_class_regr
+                iter_num = 0
+                start_time = time.time()
+
+                if curr_loss < best_loss:
+                    if C.verbose:
+                        print('Total loss decreased from {} to {}, saving weights'.format(best_loss,curr_loss))
+                    best_loss = curr_loss
+                    model_all.save_weights(C.model_path)
+
+                break
+
+        except Exception as e:
+            print('Exception: {}'.format(e))
+            continue
