263d3588f29facbc2fefb0fbcb14342e9fbf57b5
Alexandre Attia <alexandre.attia@wanadoo.fr>
2017-06-29 15:51:20 -0700

    refactoring test file with functions useable for notebooks

--- test_frcnn.py
+++ test_frcnn.py
@@ -1,4 +1,3 @@
-# from __future__ import division
 import os
 import cv2
 import numpy as np
@@ -12,24 +11,8 @@ from keras import backend as K
 from keras.layers import Input
 from keras.models import Model
 from faster_rcnn import roi_helpers
-
-sys.setrecursionlimit(40000)
-
-parser = OptionParser()
-parser.add_option("-p", "--path", dest="test_path", help="Path to test data.")
-(options, args) = parser.parse_args()
-if not options.test_path:   # if filename is not given
-    parser.error('Error: path to test data must be specified. Pass --path to command line')
-
-with open('./config.pickle', 'rb') as f_in:
-    C = pickle.load(f_in)
-
-# turn off any data augmentation at test time
-C.use_horizontal_flips = False
-C.use_vertical_flips = False
-C.rot_90 = False
-
-img_path = options.test_path
+overlap_thresh = 0.2
+bbox_threshold = 0.5
 
 def format_img(img, C):
     """
@@ -42,9 +25,10 @@ def format_img(img, C):
     img = data_generators.normalize_img(img, C)
     return img, ratio
 
-# Method to transform the coordinates of the bounding box to its original size
 def get_real_coordinates(ratio, x1, y1, x2, y2):
-
+    """
+    Method to transform the coordinates of the bounding box to its original size
+    """
     real_x1 = int(round(x1 // ratio))
     real_y1 = int(round(y1 // ratio))
     real_x2 = int(round(x2 // ratio))
@@ -52,60 +36,54 @@ def get_real_coordinates(ratio, x1, y1, x2, y2):
 
     return (real_x1, real_y1, real_x2, real_y2)
 
-class_mapping = C.class_mapping
+def get_models(C):
+    """
+    Create models : rpn, classifier and classifier only
+    :param C: config object
+    :return: models
+    """
+    img_input = Input(shape=(None, None, 3))
+    roi_input = Input(shape=(C.num_rois, 4))
+    feature_map_input = Input(shape=(None, None, 1024))
 
-if 'bg' not in class_mapping:
-    class_mapping['bg'] = len(class_mapping)
+    # define the base network (resnet here)
+    shared_layers = nn.nn_base(img_input, trainable=True)
 
-class_mapping = {v: k for k, v in class_mapping.items()}
-class_to_color = {class_mapping[v]: np.random.randint(0, 255, 3) for v in class_mapping}
+    # define the RPN, built on the base layers
+    num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+    rpn_layers = nn.rpn(shared_layers, num_anchors)
 
-img_input = Input(shape=(None, None, 3))
-roi_input = Input(shape=(C.num_rois, 4))
-feature_map_input = Input(shape=(None, None, 1024))
+    # define the classifer, built on the feature map
+    classifier = nn.classifier(feature_map_input, roi_input, C.num_rois, nb_classes=len(C.class_mapping), trainable=True)
 
-# define the base network (resnet here)
-shared_layers = nn.nn_base(img_input, trainable=True)
+    model_rpn = Model(img_input, rpn_layers)
+    model_classifier_only = Model([feature_map_input, roi_input], classifier)
 
-# define the RPN, built on the base layers
-num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
-rpn_layers = nn.rpn(shared_layers, num_anchors)
+    model_classifier = Model([feature_map_input, roi_input], classifier)
 
-# define the classifer, built on the feature map
-classifier = nn.classifier(feature_map_input, roi_input, C.num_rois, nb_classes=len(class_mapping), trainable=True)
+    model_rpn.load_weights(C.model_path, by_name=True)
+    model_classifier.load_weights(C.model_path, by_name=True)
 
-model_rpn = Model(img_input, rpn_layers)
-model_classifier_only = Model([feature_map_input, roi_input], classifier)
+    model_rpn.compile(optimizer='sgd', loss='mse')
+    model_classifier.compile(optimizer='sgd', loss='mse')
+    return model_rpn, model_classifier, model_classifier_only
 
-model_classifier = Model([feature_map_input, roi_input], classifier)
-
-model_rpn.load_weights(C.model_path, by_name=True)
-model_classifier.load_weights(C.model_path, by_name=True)
-
-model_rpn.compile(optimizer='sgd', loss='mse')
-model_classifier.compile(optimizer='sgd', loss='mse')
-
-all_imgs = []
-classes = {}
-bbox_threshold = 0.5
-overlap_thresh = 0.2
-
-for idx, img_name in enumerate(sorted(os.listdir(img_path))):
-    if not img_name.lower().endswith(('.bmp', '.jpeg', '.jpg', '.png', '.tif', '.tiff')):
-        continue
-    print(img_name)
-    st = time.time()
-    filepath = os.path.join(img_path,img_name)
-
-    img = cv2.imread(filepath)
-
+def detect_predict(pic, C, model_rpn, model_classifier, model_classifier_only, class_mapping, class_to_color, print_dets=False):
+    """
+    Detect and predict object in the picture
+    :param pic: picture numpy array
+    :param C: config object
+    :params model_*: models from get_models function
+    :params class_*: mapping and colors, need to be loaded to keep the same colors/classes 
+    :return: picture with bounding boxes 
+    """
+    img = pic 
     X, ratio = format_img(img, C)
 
     img_scaled = np.transpose(X.copy()[0, (2, 1, 0), :, :], (1, 2, 0)).copy()
     img_scaled[:, :, 0] += 123.68
     img_scaled[:, :, 1] += 116.779
     img_scaled[:, :, 2] += 103.939
-    
     img_scaled = img_scaled.astype(np.uint8)
 
     if K.image_dim_ordering() == 'tf':
@@ -124,7 +102,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
     # apply the spatial pyramid pooling to the proposed regions
     bboxes = {}
     probs = {}
-
+    # print(class_mapping)
     for jk in range(R.shape[0]//C.num_rois + 1):
         ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
         if ROIs.shape[1] == 0:
@@ -199,6 +177,39 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
             cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
             cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
 
-    print('Elapsed time = {}'.format(time.time() - st))
-    print(all_dets)
-    cv2.imwrite('./results_test/test_{}C.png'.format(idx),img)
+    if print_dets:
+        print(all_dets)
+    return img
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-p", "--path", dest="test_path", help="Path to test data.")
+    (options, args) = parser.parse_args()
+    if not options.test_path:   # if filename is not given
+        parser.error('Error: path to test data must be specified. Pass --path to command line')
+
+    ## Load config object
+    with open('./config.pickle', 'rb') as f_in:
+        C = pickle.load(f_in)
+
+    # turn off any data augmentation at test time
+    C.use_horizontal_flips = False
+    C.use_vertical_flips = False
+    C.rot_90 = False
+    model_rpn, model_classifier, model_classifier_only = get_models(C)
+    class_mapping = C.class_mapping
+    if 'bg' not in class_mapping:
+        class_mapping['bg'] = len(class_mapping)
+    class_mapping = {v: k for k, v in class_mapping.items()}
+    class_to_color = {class_mapping[v]: np.random.randint(0, 255, 3) for v in class_mapping}
+
+    for idx, img_name in enumerate(sorted(os.listdir(options.test_path))):
+        if not img_name.lower().endswith(('.bmp', '.jpeg', '.jpg', '.png', '.tif', '.tiff')):
+            continue
+        print(img_name)
+        filepath = os.path.join(options.test_path,img_name)
+        img = cv2.imread(filepath)
+        st = time.time()
+        img = detect_predict(img, C, model_rpn, model_classifier, model_classifier_only, class_mapping, class_to_color, True)
+        print('Elapsed time = {}'.format(time.time() - st))
+        cv2.imwrite('./results_test/result_{}.png'.format(img_name.replace('.png', '')),img)
